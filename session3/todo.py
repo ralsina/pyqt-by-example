91dec667f099f4efe835078f9387d738bffeddec
Roberto Alsina <ralsina@hp.(none)>
2009-03-04 09:35:29 -0200

    Starting session 3

---
+++ session3/todo.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""A simple backend for a TODO app, using Elixir"""
+
+import os
+from elixir import *
+
+dbdir=os.path.join(os.path.expanduser("~"),".pyqtodo")
+dbfile=os.path.join(dbdir,"tasks.sqlite")
+
+class Task(Entity):
+    """
+    A task for your TODO list.
+    """
+    using_options(tablename='tasks')
+    text = Field(Unicode,required=True)
+    date = Field(DateTime,default=None,required=False)
+    done = Field(Boolean,default=False,required=True)
+    tags  = ManyToMany("Tag")
+    
+    def __repr__(self):
+        return "Task: "+self.text
+
+
+class Tag(Entity):
+    """
+    A tag we can apply to a task.
+    """
+    using_options(tablename='tags')
+    name = Field(Unicode,required=True)
+    tasks = ManyToMany("Task")
+    
+    def __repr__(self):
+        return "Tag: "+self.name
+
+def initDB():
+    if not os.path.isdir(dbdir):
+        os.mkdir(dbdir)
+    metadata.bind = "sqlite:///%s"%dbfile
+    setup_all()
+    if not os.path.exists(dbfile):
+        create_all()
+    
+def main():
+    
+    # Initialize database
+    initDB()
+    
+    # Create two tags
+    green=Tag(name=u"green")
+    red=Tag(name=u"red")
+    
+    #Create a few tags and tag them
+    tarea1=Task(text=u"Buy tomatos",tags=[red])
+    tarea2=Task(text=u"Buy chili",tags=[red])
+    tarea3=Task(text=u"Buy lettuce",tags=[green])
+    tarea4=Task(text=u"Buy strawberries",tags=[red,green])
+    session.commit()
+    
+    print "Green Tasks:"
+    print green.tasks
+    print
+    print "Red Tasks:"
+    print red.tasks
+    print
+    print "Tasks with l:"
+    print [(t.id,t.text,t.done) for t in Task.query.filter(Task.text.like(ur'%l%')).all()]
+
+if __name__ == "__main__":
+    main()
