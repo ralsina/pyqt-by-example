3bf8bbae40f23f10c0d409cb966f2f5050e121bd
Roberto Alsina <ralsina@hp.(none)>
2009-03-05 00:01:35 -0200

    Try to make Elixir 0.5 and 0.6 work

--- session1/todo.py
+++ session1/todo.py
@@ -33,6 +33,9 @@ class Tag(Entity):
     def __repr__(self):
         return "Tag: "+self.name
 
+
+saveData=None
+
 def initDB():
     if not os.path.isdir(dbdir):
         os.mkdir(dbdir)
@@ -40,7 +43,20 @@ def initDB():
     setup_all()
     if not os.path.exists(dbfile):
         create_all()
+        
+    # This is so Elixir 0.5.x and 0.6.x work
+    # Yes, it's kinda ugly, but needed for Debian 
+    # and Ubuntu and other distros.
     
+    global saveData
+    import elixir
+    if elixir.__version__ < "0.6":
+        saveData=session.flush
+    else:
+        saveData=session.commit
+        
+    
+
 def main():
     
     # Initialize database
